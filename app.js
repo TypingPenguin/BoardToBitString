0b706e9fd4aa754181a88dffbf47f675b048fbd2
TypingPenguin <Floris.kromwijk@gmail.com>
2023-01-08 15:20:04 +0100

    Ascii feature + update onclick

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+const MARK1 = "X";
+const MARK2 = "O"
+const EMPTY = "~";
+const SEPERATOR = "---+---+---+---+---+---+---+---"
+
+
+
+window.onload = function(){
+    console.log("Clearing page on load.")
+    clearBoxes();
+};
+
+
+
+
 function clearBoxes(){
-    console.log("beginning clearing");
+    console.log("Beginning clearing.");
     let checkboxes= document.querySelectorAll('input');
     checkboxes.forEach((cb) => {
         cb.checked = false;
@@ -9,7 +24,9 @@ function clearBoxes(){
     document.getElementById('player1').value = "";
     document.getElementById('player2').value = "";
     document.getElementById('board').value = "";
-    console.log("cleared");
+    document.getElementById('player1ascii').value = "";
+    document.getElementById('boardascii').value = "";
+    console.log("Succesfully cleared.");
 }
 
 
@@ -41,8 +58,11 @@ function generate(){
     console.log(undeterminate);
 
     var stringPlayer1 = "";
+    var stringPlayer1Ascii = ""
     var stringPlayer2 = "";
+    var stringPlayer2Ascii = "";
     var boardString = "";
+    var boardStringAscii = "";
     let x = 0;
     let pos = "1";
     let neg = "0";
@@ -50,8 +70,30 @@ function generate(){
         if(checked[x] == i){
             stringPlayer1 = pos.concat(stringPlayer1);
             x++;
+
+            if (i%8 == 0 && i!=0){
+                stringPlayer1Ascii += "\n";
+                stringPlayer1Ascii += SEPERATOR + "\n";
+            }
+            if (i%8 == 7 && i!=0){
+                stringPlayer1Ascii += " " + MARK1;
+
+            } else{
+                stringPlayer1Ascii += " " + MARK1 +" |";
+            }
+
         } else{
             stringPlayer1 = neg.concat(stringPlayer1);
+
+            if (i%8 == 0 && i!=0){
+                stringPlayer1Ascii += "\n";
+                stringPlayer1Ascii += SEPERATOR + "\n";
+            }
+            if (i%8 == 7 && i!=0){
+                stringPlayer1Ascii += " " + EMPTY;
+            } else{
+                stringPlayer1Ascii += " " + EMPTY + " |";
+            }
         }
     }
 
@@ -60,8 +102,29 @@ function generate(){
         if(undeterminate[x] == i){
             stringPlayer2 = pos.concat(stringPlayer2);
             x++;
+
+            if (i%8 == 0 && i!=0){
+                stringPlayer2Ascii += "\n";
+                stringPlayer2Ascii += SEPERATOR + "\n";
+            }
+            if (i%8 == 7 && i!=0){
+                stringPlayer2Ascii += " " + MARK2;
+
+            } else{
+                stringPlayer2Ascii += " " + MARK2 +" |";
+            }
         } else{
             stringPlayer2 = neg.concat(stringPlayer2);
+
+            if (i%8 == 0 && i!=0){
+                stringPlayer2Ascii += "\n";
+                stringPlayer2Ascii += SEPERATOR + "\n";
+            }
+            if (i%8 == 7 && i!=0){
+                stringPlayer2Ascii += " " + EMPTY;
+            } else{
+                stringPlayer2Ascii += " " + EMPTY + " |";
+            }
         }
     }
 
@@ -78,6 +141,18 @@ function generate(){
         }
     }
 
+    for(var i = 0; i < stringPlayer1Ascii.length ; i++){
+        if (stringPlayer1Ascii[i] === MARK1){
+            boardStringAscii += stringPlayer1Ascii[i];
+        } else if (stringPlayer2Ascii[i] == MARK2){
+            boardStringAscii += stringPlayer2Ascii[i];
+        } else {
+            boardStringAscii += stringPlayer1Ascii[i];
+        }
+    }
+
+    
+
     console.log("These are the strings in following order: Player1, Player2, Board:");
     console.log(stringPlayer1);
     console.log(stringPlayer2);
@@ -87,6 +162,9 @@ function generate(){
     document.getElementById('player2').value = stringPlayer2;
     document.getElementById('board').value = boardString;
 
+    document.getElementById('player1ascii').value = stringPlayer1Ascii;
+    document.getElementById('boardascii').value = boardStringAscii;
+
 }
 
 function copy(id){
@@ -95,7 +173,58 @@ function copy(id){
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
 
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
+    //var tooltip = document.getElementById("myTooltip");
+    //tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function generateFromString(){
+    var player1 = document.getElementById('player1').value
+    var player2 = document.getElementById('player2').value
+    var board = document.getElementById('board').value
+
+    var player1Array = []
+    var player2Array = []
+
+    //Create array of bit pos for player1
+    if (player1 !== ""){
+        for (let i = player1.length-1; i >=0; i--){
+            if (player1[i] === "1"){
+                player1Array.push(Math.abs(i-63));
+            }
+        }
+    }
+
+    //Create array of bit pos for player2
+    if (player2 !== ""){
+        for (let i = player2.length-1; i >=0; i--){
+            if (player2[i] === "1"){
+                player2Array.push(Math.abs(i-63));
+            }
+        }
+    }
+
+
+    let checkboxes= document.querySelectorAll('input');
+
+    player1Array.forEach(element => {
+        checkboxes.forEach(cb => {
+            if (cb.value == element){
+                cb.readOnly = false;
+                cb.checked = true;
+            }
+        });
+    });
+
+    player2Array.forEach(element => {
+        checkboxes.forEach(cb => {
+            if (cb.value == element){
+                ts(cb);
+            }
+        });
+    });
+
+    generate();
+    console.log(player2);
+
 }
 
